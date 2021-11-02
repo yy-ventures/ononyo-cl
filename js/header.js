ef510ce8d91ffb5835ac7e3d128576f5e7dcc8a2
Mamun <mmamun.roshid@gmail.com>
2021-11-02 17:47:36 +0600

    issue fixing frontend

--- js/header.js
+++ js/header.js
@@ -2,8 +2,21 @@ $(document).ready(function () {
     var $magic = $(".magic"),
       magicWHalf = $magic.width() / 2;
     $(document).on("mousemove", function (e) {
+    console.log('e.pageX');
+    console.log(e.pageX);
+    console.log('magicWHalf');
+    console.log(magicWHalf);
+
+
+
+
       if (e.pageX - magicWHalf >= 0) {
-        $magic.css({ "left": e.pageX - magicWHalf });
+        if(e.pageX >=1010){
+          $magic.css({ "left":null,right:0 });
+        }else{
+           $magic.css({ "left": e.pageX - magicWHalf });
+        }
+        
       } else {
         $magic.css({ "left": 0 });
       }
